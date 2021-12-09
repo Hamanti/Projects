056cf148287f3477af8302a0ebdc607f964f4933
Ilnar Ihsanov <iixsanov@inbox.ru>
2021-12-10 00:20:24 +0300

    Сделана валидации форм расчета стоимости

--- js/modal.js
+++ js/modal.js
@@ -1,35 +1,68 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  /* MASKED */
   $('#phoneinput').mask('+7 (999) 999-99-99');
-  /* AJAX */
-  $('#offer-form').on('submit', function(event) {
+  $('#phoneinput2').mask('+7 (999) 999-99-99');
+
+  /* VALIDATE COST*/
+  $('#repair-form').validate({
+    rules: {
+      username: "required",
+      userphone: "required",
+    },
+    messages: {
+      username: "Пожалуйста, укажите имя!",
+      userphone: "Пожалуйста, укажите телефон!",
+    }
+    
+  });
+  /* AJAX CALL*/
+  $('#offer-form').on('submit', function (event) {
     event.preventDefault();
     $.ajax({
-      url: 'mail.php',
+      url: '../php/recall.php',
       type: 'POST',
       data: $(this).serialize(),
-      success: function(data) {
-        
+      success: function (data) {
+
         $('#modal').addClass('modal_active');
         $('#modaltext').html(data);
-        event.target.reset(); 
+        event.target.reset();
       }
-    }); 
+    });
   });
 
- 
+  /* AJAX COST*/
+
+    $('#repair-form').on('submit', function (event) {
+      event.preventDefault();
+      $.ajax({
+        url: '../php/sumcost.php',
+        type: 'POST',
+        data: $(this).serialize(),
+        success: function (data) {
+  
+          $('#modal').addClass('modal_active');
+          $('#modaltext').html(data);
+          event.target.reset();
+        }
+      });
+    });
+  
+
+
 
   /* Модальное окно */
-  $('#button__call').click(function() {
+  $('#button__call').click(function () {
     $('#modal').addClass('modal_active')
     $('#modaltext').html('Модальное окно');
   });
 
-  $('#button__close').click(function() {
+  $('#button__close').click(function () {
     $('#modal').removeClass('modal_active')
   });
 
-  $('#uparrow').click(function() {
-		jQuery("html,body").animate({scrollTop:0},"slow");
+  $('#uparrow').click(function () {
+    jQuery("html,body").animate({ scrollTop: 0 }, "slow");
   });
 
 
